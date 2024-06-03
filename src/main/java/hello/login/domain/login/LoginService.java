21cba1267273b64309ab56de807db61db87d8ca5
InimitablePioneer <seungwonpioneer@gmail.com>
2024-06-03 15:42:11 +0900

    JUL 3 at work via WIN

---
+++ src/main/java/hello/login/domain/login/LoginService.java
@@ -0,0 +1,24 @@
+package hello.login.domain.login;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * @return null이면 로그인 실패
+     */
+    public Member login(String loginId, String password) {
+        Optional<Member> findMemberOptional = memberRepository.findByLoninId(loginId);
+
+    }
+
+}
