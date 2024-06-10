34daba02d46d7009bf112225e0dc97e72ceb3718
InimitablePioneer <seungwonpioneer@gmail.com>
2024-06-10 15:42:54 +0900

    JUN 10 at work via WIN

--- src/main/java/hello/login/domain/member/Member.java
+++ src/main/java/hello/login/domain/member/Member.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.NotEmpty;
 @Data
 public class Member {
 
-    private Long id;//시스템에서 쓰는 id 값
+    private Long id;//시스템에서 쓰는 id 값 //멤버 아이디
 
     @NotEmpty
     private String loginId; //로그인시 아이디
--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
@@ -27,7 +29,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult) {
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
@@ -40,6 +42,9 @@ public class LoginController {
         }
 
         //로그인 성공처리 TODO
+        //쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
 
         return "redirect:/";
 
