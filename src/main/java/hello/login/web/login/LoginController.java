ce5b55f387ba1baeb3034b7532c0f7c9a905cb8c
InimitablePioneer <seungwonpioneer@gmail.com>
2024-06-12 23:52:32 +0900

    JUN12 at home via MAC

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -49,6 +49,17 @@ public class LoginController {
         return "redirect:/";
 
 
+    }
 
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
