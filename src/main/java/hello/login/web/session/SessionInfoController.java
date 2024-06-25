67d956a44e6e270aa853841a6ecd313d4d6df4d7
InimitablePioneer <seungwonpioneer@gmail.com>
2024-06-25 15:11:35 +0900

    JUN25 at work via WIN

--- src/main/java/hello/login/web/session/SessionInfoController.java
+++ src/main/java/hello/login/web/session/SessionInfoController.java
@@ -1,6 +1,22 @@
 package hello.login.web.session;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+@RestController //좀더 학습
 public class SessionInfoController {
+    @GetMapping("/session-info")
+    public String sessionInfo(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "세션이 없습니다.";
+        }
+    }
     public static void main(String[] args) {
 
     }
